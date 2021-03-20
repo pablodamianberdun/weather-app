d58166a7ffd23e21dadb8bee16abedf5b28283e0
Pablo Damian Berdun <pablodamianberdun@gmail.com>
2021-03-20 15:36:08 -0300

    Added a custom background image for each card related to its city

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Header";
 import WeatherCard from "./components/WeatherCard";
-import Footer from './components/Footer'
-import styled from "styled-components";
-import background from './img/sky-50e9d54143_1280.jpg'
 
-const Main = styled.div ` 
-	min-height: 100vh;
-    background-image: url(${background});
-    background-repeat: no-repeat;
-	background-attachment: fixed;
-	background-position: center;
-	background-size: cover;
-	position: relative;
-`
 
 function App() {
     const [city, setCity] = useState({});
@@ -24,23 +12,23 @@ function App() {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.name}&units=metric&appid=fa17ec1c7d684c94614d3a196892c3d0`;
 
     if (getApi) {
-        getWeather();
-        async function getWeather() {
-            const response = await fetch(url);
+		async function getWeather() {
+			const response = await fetch(url);
             const data = await response.json();
             setGetApi(false);
             if (data.cod === "404") {
-                setError(true);
+				setError(true);
             } else {
-                setError(false);
+				setError(false);
                 data.id = city.id;
                 setWeather([...weather, data]);
             }
         }
+		getWeather();
     }
 
     return (
-        <Main>
+        <Fragment>
             <Header
                 title="Weather App"
                 setCity={setCity}
@@ -56,12 +44,12 @@ function App() {
                         <WeatherCard
                             key={weatherCity.id}
                             weatherCity={weatherCity}
+							bgColor="red"
                         />
                     ))}
                 </div>
             ) : null}
-			<Footer/>
-        </Main>
+        </Fragment>
     );
 }
 
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,59 +1,91 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 const Card = styled.div`
-    color: white;
-    background-color: rgba(8, 104, 205, 0.5);
     text-align: center;
-	padding: 30px;
-	margin-right: auto;
-	margin-left: auto;
-    margin-bottom: 60px;
-    width: 100%;
-    height: 20rem;
-    border-radius: 10px;
+    margin: 5px;
+    height: auto;
 
-	display: flex;
-	flex-direction: column;
-	align-items:center;	
-	justify-content: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 
     @media (min-width: 768px) {
         width: 30%;
     }
 `;
 
-const CityName = styled.h1 ` 
-	display: block;
-	font-size: 8vw;
-	margin: 0;
+const BACKGROUND = styled.div`
+    position: relative;
+    height: auto;
 
-	@media (min-width: 768px) {
-		font-size: 3vw;
-	}
+    img {
+        border-radius: 10px;
+        width: 100%;
+        filter: brightness(0.4);
+    }
+`;
+const WEATHERDATA = styled.div`
+    position: absolute;
+    height: auto;
+	color: white;
 
-	@media (min-width: 900px) {
-		font-size: 2.5vw;
-	}
+    h1 {
+        display: block;
+        font-size: 8vw;
+        margin: 0;
 
-`
+        @media (min-width: 768px) {
+            font-size: 3vw;
+        }
 
-const IMG = styled.img`
-	width: 30%;
-`
+        @media (min-width: 900px) {
+            font-size: 2.5vw;
+        }
+    }
 
-function WeatherCard({ weatherCity }) {
-    const { main, name, weather } = weatherCity;
-	console.log(weatherCity)
+    img {
+        height: 100%;
+        width: 100%auto;
+        size: cover;	
+    }
+`;
+
+function WeatherCard(props) {
+    const { main, name, weather } = props.weatherCity;
+    const [urlImg, setUrlImg] = useState("");
+
+    useEffect(() => {
+        const getCardImage = async () => {
+            const url = `https://pixabay.com/api/?key=19949153-2db7b3c8211ebbbd16f59e64d&q=${name}&orientation=vertical&image_type=photo&pretty=true`;
+            const response = await axios.get(url);
+            const links = response.data.hits;
+            const num = Math.floor(Math.random() * links.length);
+            const link = links[num];
+            setUrlImg(link.largeImageURL);
+        };
+
+        getCardImage();
+    }, []);
 
     return (
         <Card>
-            <CityName>{name}</CityName>
-            <h2>{main.temp} ºC</h2>
-            <p>Min: {main.temp_min} ºC</p>
-            <p>Max: {main.temp_max} ºC</p>
-			<IMG src={`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`} alt={weather[0].description}/>
-			<p style={{"margin": 0}}>{weather[0].main}</p>
+            <BACKGROUND>
+                <img src={urlImg} alt="background city"/>
+            </BACKGROUND>
+            <WEATHERDATA>
+                <h1>{name}</h1>
+                <h2>{main.temp} ºC</h2>
+                <p>Min: {main.temp_min} ºC</p>
+                <p>Max: {main.temp_max} ºC</p>
+                <img
+                    src={`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`}
+                    alt={weather[0].description}
+                />
+                <p style={{ margin: 0 }}>{weather[0].main}</p>
+            </WEATHERDATA>
         </Card>
     );
 }
